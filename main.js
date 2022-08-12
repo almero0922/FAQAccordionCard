3ac21c8676b454a529c910ece7f34eadcaa83b2b
almero0922 <albertomedel29@gmail.com>
2022-08-11 21:01:29 -0500

    Active states for answers and arrow icons

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 const questions = document.querySelectorAll('.container-single-faq');
 const clickedColor = getComputedStyle(document.documentElement).getPropertyValue('--veryDarkGrayishBlue');
+const answers = document.querySelectorAll('.answers');
 
 questions.forEach((el, i) => {
     el.addEventListener('click', () => {
+        checkInactive();
         const childAnswer = el.querySelector('.answers');
         const childQuestion = el.querySelector('.questions');
+        const iconArrow = el.querySelector('.icon-arrow');
         childAnswer.classList.toggle('inactive');
         childQuestion.style.color = 'black';
         childQuestion.style.fontWeight = 'bold';
+        iconArrow.classList.add('icon-arrow-top');
     })
 })
+
+function checkInactive(){
+    answers.forEach( element => {
+        const parent = element.parentElement;
+        const childIconArrow = parent.querySelector('.icon-arrow');
+        if(!element.classList.contains('inactive')){
+            element.classList.add('inactive');
+            childIconArrow.classList.remove('icon-arrow-top');
+        }
+    })
+}
